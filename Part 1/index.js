d81fef99f7986e2184c5edb8b84eaf155cad78dd
Felipe Cristiano <lipimzod@users.noreply.github.com>
2025-03-13 08:12:26 -0300

    Initial commit - Basics Review 2

--- Part 1/index.js
+++ Part 1/index.js
@@ -1,7 +1,13 @@
 console.log('Testing HTML')
 
-alert('Test 2') // Not identified by Node.
+// alert('Test 2') // Alert is not identified by Node.
 
 let name
 name = 'Felipe' 
 console.log(name)
+
+const numOne = 78
+const numTwo = 86
+let result = numOne * numTwo
+console.log('The operation result is' + result)
+console.log(`The operation result is ${result}`)
