10803700cf729a89f9d79f946ed1d879d29e2bee
Lipimzodia <cr.felipe2@gmail.com>
2025-03-18 15:09:38 -0300

    Initial commit - Basics Review 4

--- Part 2/concepts.js
+++ Part 2/concepts.js
@@ -1,6 +1,22 @@
 let num1 = 10.28932543
 let num2 = 2.5
 let temp = num1 * 'Hello'
-
 console.log(num1.toFixed(1))
 console.log(temp)
+console.log(Number.isNaN(temp))
+
+let num3 = 9.546745
+let num4 = Math.floor(num3)
+let num5 = Math.ceil(num1)
+let num6 = Math.round(num3)
+console.log(num4)
+console.log(num5)
+console.log(num6)
+console.log(Math.random()*10)
+
+const data = ['Cookie', 'Brownie', 'Red Velvet', 'Lollipop']
+data.push('Lemon Pie')
+data.unshift('Ice Cream')
+console.log(data)
+console.log(data.length)
+console.log(data[1])
