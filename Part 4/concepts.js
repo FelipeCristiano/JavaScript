f7786fdfc98be51769721cd0d719c15112842052
Lipimzodia <cr.felipe2@gmail.com>
2025-03-26 14:21:45 -0300

    Basics Review 7

--- Part 4/concepts.js
+++ Part 4/concepts.js
@@ -4,3 +4,11 @@ console.log(20 == 20)
 console.log(20 === '20')
 console.log(20 != 20)
 
+const time = 14
+if (time >= 0 && time <= 11) {
+    console.log('Good Morning!')
+} else if (time >= 12 && time <= 17) {
+    console.log('Good Afternoon')
+} else {
+    console.log('Good Evening')
+}
